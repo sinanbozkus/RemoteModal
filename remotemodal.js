f6f830f2579c8f80b6fd51cf141ba92c10a6dce1
Sinan BOZKUŞ <sinan.bozkus@gmail.com>
2019-05-18 17:53:29 +0300

    Added remotemodal.js

---
+++ remotemodal.js
@@ -0,0 +1,22 @@
+var RemoteModal = function (url, options) {
+
+    if (!$('#remoteModal').length) {
+        $("body").append('<div id="remoteModal" class="modal" tabindex="-1" role="dialog"></div>');
+    }
+
+    $('body #remoteModal').html("");
+
+    $('body #remoteModal').load(url, function () {
+
+        if (options === undefined) {
+            $("#remoteModal").modal({ show: true });
+        } else {
+            $("#remoteModal").modal(options);
+        }
+
+    });
+};
+
+var DisposeRemoteModal = function () {
+    $('body #remoteModal').modal('hide').modal('dispose');
+};
